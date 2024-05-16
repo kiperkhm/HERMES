d048056d796a5808e8dc37b207d60a5f8dcee7a9
NewbieHeater <103471494+NewbieHeater@users.noreply.github.com>
2024-05-16 20:40:33 +0900

    유닛코드

---
+++ UnitCode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitCode
+{
+    Vengeful_Warrior,
+    Grudge_Archer,
+    BasicSummoner,
+    Slime
+}
